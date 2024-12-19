433e267dfed029253756f536fd2c967476717e8f
002976 <tiffanybyun0209@gmail.com>
2024-12-19 15:59:24 +0900

    update

--- cal_healthdata.c
+++ cal_healthdata.c
@@ -62,27 +62,28 @@ void printHealthData(const HealthData* health_data) {
 	
 	// ToCode: to print out the saved history of exercises
 	printf("=========================== History of Exercise =======================\n");
-  
-  
+	printf("%i\n", health_data->exercise_count);
     printf("=======================================================================\n");
-
+    
+    
     // ToCode: to print out the saved history of diets
     printf("============================= History of Diet =========================\n");
-
-
+    
+    
     printf("=======================================================================\n");
-
-
+    
+    
 	// ToCode: to print out the saved history of calories including basal metabolic rate, 
 	// total calories burned, total calories intake, and the remaining calories
 	printf("============================== Total Calories =========================\n");
- 
- 
+	
+	
+	
     printf("=======================================================================\n \n");
     
-	
+    
 	// ToCode: to print out the recommendtaion depending on the current total calories burned and intake    
     
     
-	 printf("=======================================================================\n");
+	printf("=======================================================================\n");
 }
